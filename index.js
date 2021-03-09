5080ab4e603f6cd0b860ca35dc694a52191bb24f
JMalmqvist <78379021+JMalmqvist@users.noreply.github.com>
2021-03-09 15:48:59 -0700

    Main Index.HTML
    This is the main page for the Travel Canada website.

---
+++ index.js
@@ -0,0 +1,37 @@
+function flightFocusFunction(){
+    var x=document.getElementById("flight")
+        x.style.opacity=1;
+}
+
+function flightBlurFunction(){
+    var x=document.getElementById("flight")
+        x.style.opacity=0;
+}
+
+function hotelFocusFunction(){
+    var x=document.getElementById("hotel")
+        x.style.opacity=1;
+}
+
+function hotelBlurFunction(){
+    var x=document.getElementById("hotel")
+        x.style.opacity=0;
+}
+
+function rentalFocusFunction(){
+    var x=document.getElementById("rental")
+        x.style.opacity=1;
+}
+
+function rentalBlurFunction(){
+    var x=document.getElementById("rental")
+        x.style.opacity=0;
+}
+// function nameFocusFunction() {
+//   var x = document.getElementById("namedesc");
+//   x.style.opacity=1;
+// }
+// function nameBlurFunction() {
+//   var x = document.getElementById("namedesc");
+//   x.style.opacity=0;
+// }
